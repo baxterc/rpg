1b125294fce28c7911cb4aa7b56d6425fe3215e8
Charlie Baxter and Shradha Pulla <charlie+shradha@>
2016-09-15 13:36:42 -0700

    add roles to users

--- src/DotNetRPG/Controllers/RoleController.cs
+++ src/DotNetRPG/Controllers/RoleController.cs
@@ -15,13 +15,15 @@ namespace DotNetRPG.Controllers
     {
         private readonly ApplicationDbContext _db;
         private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly UserManager<ApplicationUser> _userManager;
 
         //ALWAYS use a constructor in Controllers
 
-        public RoleController(ApplicationDbContext db, RoleManager<IdentityRole> roleManager)
+        public RoleController(ApplicationDbContext db, RoleManager<IdentityRole> roleManager, UserManager<ApplicationUser> userManager)
         {
             _db = db;
             _roleManager = roleManager;
+            _userManager = userManager;
         }
         //Need a constructor to redefine RoleManager because it's automatically null & doesn't update values when program is run
 
@@ -56,6 +58,23 @@ namespace DotNetRPG.Controllers
             var list = _db.Roles.OrderBy(r => r.Name).ToList().Select(rr => new SelectListItem { Value = rr.Name.ToString(), Text = rr.Name}).ToList();
             ViewBag.Roles = list;
             return View();
+        }
+        
+        [HttpPost]
+        public async Task<IActionResult> AddUser(string UserName, string RoleName)
+        {
+            ApplicationUser user = _db.Users.Where(u => u.UserName.Equals(UserName, StringComparison.CurrentCultureIgnoreCase)).FirstOrDefault();
+            IdentityResult result = await _userManager.AddToRoleAsync(user, RoleName);
+            if (result.Succeeded)
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                var list = _db.Roles.OrderBy(r => r.Name).ToList().Select(rr => new SelectListItem { Value = rr.Name.ToString(), Text = rr.Name }).ToList();
+                ViewBag.Roles = list;
+                return View();
+            }
         }
     }
 }
