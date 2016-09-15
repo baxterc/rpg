a830a3aef49509202e0e495931886416d2394f20
Charlie Baxter and Shradha Pulla <charlie+shradha@>
2016-09-15 15:12:44 -0700

    Create DotNetRPG.Test file, and run a sample test to find username

---
+++ DotNetRPG.Tests/AccountTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DotNetRPG.Models;
+using Xunit;
+
+namespace DotNetRPG.Tests
+{
+    public class AccountTest
+    {
+       [Fact]
+       public void GetUserNameTest()
+        {
+            var user = new ApplicationUser();
+            user.UserName = "TestUser";
+            var result = user.UserName;
+            Assert.Equal("TestUser", result);
+        }
+    }
+}
